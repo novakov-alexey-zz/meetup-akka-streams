f9c0e7c429feca2e2089119b950c737635e4555e
Alexey Novakov <an@datawerks.com>
2017-10-31 18:56:13 +0100

    use scalikejdbc instead of mybatis; create draft of graph stages to use instead of Actors

--- src/main/scala/meetup/streams/ex2/exchange/Config.scala
+++ src/main/scala/meetup/streams/ex2/exchange/Config.scala
@@ -1,20 +1,26 @@
 package meetup.streams.ex2.exchange
 
 import com.google.inject.{AbstractModule, Guice, Injector}
-import meetup.streams.ex2.exchange.dal.{IOrderDao, OrderDaoImpl, OrderDaoMapping}
-import org.mybatis.scala.config.Configuration
-import org.mybatis.scala.session.SessionManager
+import meetup.streams.ex2.exchange.dal.{OrderDao, OrderDaoImpl}
+import scalikejdbc.{ConnectionPool, ConnectionPoolSettings}
 
 object Config {
-  def createMybatisConfig(): Configuration = Configuration("mybatis.xml").
-    addSpace("meetup.akka.dal.OrderDao") { space ⇒
-      space ++= OrderDaoMapping
-    }
-
-  val persistenceContext: SessionManager = createMybatisConfig().createPersistenceContext
   val injector: Injector = Guice.createInjector(new OrderProcessorModule)
 }
 
 class OrderProcessorModule extends AbstractModule {
-  override def configure(): Unit = bind(classOf[IOrderDao]) to classOf[OrderDaoImpl]
+
+  override def configure(): Unit =
+    bind(classOf[OrderDao]) toInstance {
+      val settings = ConnectionPoolSettings(
+        initialSize = 5,
+        maxSize = 20,
+        connectionTimeoutMillis = 3000L,
+        validationQuery = "select 1 from dual")
+
+      ConnectionPool.add("orders",
+        "jdbc:mysql://localhost:3306/somedb?autoReconnect=true&amp;useSSL=false", "root", "root", settings)
+
+      new OrderDaoImpl("orders")
+    }
 }
