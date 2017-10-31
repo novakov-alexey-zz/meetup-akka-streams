54e247ff6ac61f98fa499fb3dc3b756cdac22225
Alexey Novakov <an@datawerks.com>
2017-10-31 19:34:19 +0100

    add docker compose for mysql and db schema minor refactoring

--- src/main/scala/meetup/streams/ex2/exchange/Test.scala
+++ src/main/scala/meetup/streams/ex2/exchange/Test.scala
@@ -8,11 +8,11 @@ import scala.concurrent.Future
 
 object Test extends App {
   val source = Source(1 to 100)
-  val concat = Flow[Int].filter(_ % 2 == 0)
+  val filter = Flow[Int].filter(_ % 2 == 0)
   //emits result when the upstream completes
   val sink = Sink.fold[Int, Int](0)(_ + _)
 
-  val g: RunnableGraph[Future[Int]] = source.via(concat).toMat(sink)(Keep.right)
+  val g: RunnableGraph[Future[Int]] = source.via(filter).toMat(sink)(Keep.right)
   val sum: Future[Int] = g.run
   sum.foreach(print)
 }
