b8ec6d8f89494546de9507d8fee3ae0a74a4d96e
Alexey Novakov <an@datawerks.com>
2017-11-23 22:18:30 +0100

    update test

--- src/main/scala/meetup/streams/ex2/exchange/Test.scala
+++ src/main/scala/meetup/streams/ex2/exchange/Test.scala
@@ -5,6 +5,8 @@ import meetup.streams.ex2.exchange.Common._
 
 import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.Future
+import scala.concurrent.duration._
+import scala.util.Random
 
 object Test extends App {
   val source = Source(1 to 100)
@@ -15,4 +17,22 @@ object Test extends App {
   val g: RunnableGraph[Future[Int]] = source.via(filter).toMat(sink)(Keep.right)
   val sum: Future[Int] = g.run
   sum.foreach(print)
+
+
+  // An example from Colin Breck article:
+
+  val random = new Random
+
+  case class Status(text: String = "Hi I am status message")
+
+  case class Sample(currentTime: Long, nextFloat: Float)
+
+  val status =
+    Source.tick(0 minute, 5 seconds, ())
+      .map(_ => Status)
+
+  Source.tick(0 milliseconds, 1 second, ())
+    .map(_ => Sample(System.currentTimeMillis(), random.nextFloat()))
+    .merge(status)
+    .runWith(Sink.foreach(println))
 }
